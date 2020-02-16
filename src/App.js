75aad4acd8aab1f7cc1aee9c9c8bee91f10a9c12
Sean Kurtz <sean_kurtz@hotmail.com>
2020-02-15 20:38:50 -0600

    Basic setup of Dashboard.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,242 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
 
-function App() {
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { Card, CardMedia, CardContent } from '@material-ui/core';
+import { ListAlt, Settings, AttachMoney } from '@material-ui/icons/ListAlt';
+import { ListItemText, ListItemIcon } from '@material-ui/core';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import SettingsApplicationsIcon from '@material-ui/icons/SettingsApplications';
+import LocalAtmIcon from '@material-ui/icons/LocalAtm';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+const listItems = [
+  {
+    id: 1,
+    name: "Banana",
+    image: "banana.jpg",
+    price: 1.41
+  },
+  {
+    id: 2,
+    name: "Apple",
+    image: "apple.jpg",
+    price: 2.11
+  },
+  {
+    id: 3,
+    name: "Roast",
+    image: "roast.jpg",
+    price: 16.44
+  },
+  {
+    id: 4,
+    name: "Chicken",
+    image: "chicken.jpg",
+    price: 25.01
+  },
+]
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+  card: {
+    maxWidth: 345,
+    margin: theme.spacing(3)
+  },
+  cardMedia: {
+    height: 140
+  },
+  cardInfo: {
+    display: 'flex',
+    padding: 0
+  },
+  cardInfoLeft: {
+    padding: 0
+  }
+
+}));
+export default function App() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}>
+            <MenuIcon />
+          </IconButton>
+          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <ListAltIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inventory" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <LocalAtmIcon />
+            </ListItemIcon>
+            <ListItemText primary="Sales" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <SettingsApplicationsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={2}>
+            {listItems.map(item => (
+              <Grid item xs={12} md={6} lg={3}>
+                <Card className={classes.card}>
+                  <CardMedia className={classes.cardMedia} image={item.image} title={item.name} />
+                  <CardContent>
+                    <Container maxWidth="lg" className={classes.cardInfo}>
+                      <Container maxWidth="lg" className={classes.cardInfoLeft}>
+                        <Typography variant="h5">{item.name}</Typography>
+                        <Typography variant="body1">{"$" + item.price}</Typography>
+                      </Container>
+                      <Button variant="contained" color="secondary">X</Button>
+                    </Container>
+                  </CardContent>
+                </Card>
+              </Grid>))
+            }
+          </Grid>
+        </Container>
+
+      </main>
     </div>
   );
 }
 
-export default App;
+
